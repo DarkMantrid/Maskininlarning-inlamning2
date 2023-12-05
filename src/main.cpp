265a5de78da1e804bc365c30badf17743fda479f
Orpheus Johansson <123548945+DarkMantrid@users.noreply.github.com>
2023-12-05 15:52:57 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,17 @@
 
 namespace ml = yrgo::machine_learning;
 
-int main(void) {
-    const std::vector<std::vector<double>> input{{ 0, 1, 2, 4, 5 }, 
-                                                 { 6, 7, 8, 9, 10 }, 
-                                                 { -1, 0, 5, 2, 6 },
-                                                 { 10, 15, 2, 6, 8 }, 
-                                                 { 34, 3, 2, 5.6, 7 }};
-    ml::ConvLayer conv1{5, 3};
-    ml::PoolingLayer pool1{3, 1};
+int main() {
+    const std::vector<std::vector<double>> input{
+        { 0, 1, 2, 4, 5 },
+        { 6, 7, 8, 9, 10 },
+        { -1, 0, 5, 2, 6 },
+        { 10, 15, 2, 6, 8 },
+        { 34, 3, 2, 5.6, 7 }
+    };
+
+    ml::ConvLayer conv1{5, 3}; // Creating a ConvLayer object with an image size of 5x5 and a kernel size of 3x3
+
     conv1.Feedforward(input);
-    conv1.Print();
-    pool1.Feedforward(conv1.Output());
-    pool1.Print();
+    conv1.Print();  // Print the results or perform further operations
 }
